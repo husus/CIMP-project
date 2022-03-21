7a8846117f544f40993c5bf3e9b770824c89eac5
Susan <93041464+husus@users.noreply.github.com>
2022-03-21 17:56:13 +0100

    Add files via upload

---
+++ VoD project.R
@@ -0,0 +1,77 @@
+rm(list = ls())
+library("dplyr")
+library("data.table")
+options(scipen=999)
+
+num_users <- 1000
+
+u_id <- seq(1,num_users)
+
+u_age <- sample(c(1,2,3,4,5,6),num_users,replace=T,prob=c(0.1,0.30,0.35,0.15,0.05,0.05))
+# 1=<18 2=[18,25) 3=[25,35) 4=[35,45), 5=[45,55), 6=>55
+
+u_gender <- sample(c(1,2),num_users,replace=T,prob=c(0.6,0.4))
+# 1=M, 2=F
+
+# u_occupation <- sample(c(1,2,3,4,5),num_users,replace=T, )
+# 1=student 2=part-time 3=full-time 4=unemployed 5=retired
+
+u_weekly_utilisation <- sample(0:7,num_users,replace=T)
+# number of days using the service in a week
+
+u_sub_utilisation <- round(runif(num_users,0,1),4)
+# proportion of time spent on the service since the first subscription
+
+u_rating_given <- sample(0:5,num_users,replace=T)
+# rating on a scale from 0 to 5 given by each user to the platform
+
+u_format_pref <- sample(c(1,2,3),num_users,replace=T,prob=c(0.5,0.4,0.1))
+# 1=TV-series, 2=movies, 3=documentaries
+
+u_genre_pref <- sample(1:7,num_users,replace=T)
+# 1=action, 2=comedy, 3=romance, 4=sci-fi, 5=animation, 6=drama, 7=horror
+
+u_other_sub <- sample(0:1,num_users,replace=T)
+# binary variable where 0=not subscribed to other streaming platforms, 1=yes
+
+USERS <- data.frame(u_id, u_gender, u_age, u_weekly_utilisation, u_sub_utilisation, u_format_pref,
+                    u_genre_pref, u_rating_given, u_other_sub)
+USERS <- data.table(USERS)
+
+
+USERS$u_occupation[u_age==1] <- 1
+USERS$u_occupation[u_age==2|u_age==3] <- sample(c(2,3,4),nrow(USERS[u_age==2|u_age==3]),replace=T,prob=c(0.4,0.4,0.2))
+USERS$u_occupation[u_age==4|u_age==5] <- sample(c(2,3,4),nrow(USERS[u_age==4|u_age==5]),replace=T,prob=c(0.1,0.7,0.2))
+USERS$u_occupation[u_age==6] <- sample(c(3,5),nrow(USERS[u_age==6]),replace=T,prob=c(0.3,0.7))
+# occupation: 1=student 2=part-time 3=full-time 4=unemployed 5=retired
+# age: 1=<18 2=[18,25) 3=[25,35) 4=[35,45), 5=[45,55), 6=>55
+
+table(USERS$u_age, USERS$u_occupation)
+barplot(table(USERS$u_age, USERS$u_occupation), beside=T, legend=T, col=rainbow(6))
+
+
+
+# u_genre_pref(1=action||4=sci-fi), u_format_pref(1=series), u_age(-), u_occupation(-)
+# u_other_sub(-), u_rating_given(+), u_sub_utilisation(+), u_weekly_utilisation(+)
+# to add error from rnorm
+
+# score = u_genre_pref*80 + u_format_pref*100 + u_age(1|2)*30 - u_age(3|4|5|6)*30 
+#         + u_occupation(1|4|5)*30 - u_occupation(2|3)*30 - u_other_sub*55 + u_rating_given*50
+#         + u_sub_utilisation*130 + u_weekly_utilisation*115 + error
+
+
+USERS[,score:=u_rating_given*50+u_sub_utilisation*130+u_weekly_utilisation*115-u_other_sub*55+rnorm(1)*100]
+USERS[u_genre_pref==1|u_genre_pref==4, score:=score+80]
+USERS[u_format_pref==1, score:=score+100]
+USERS[,score:=ifelse(u_age==1|u_age==2,score+30,score-30)] 
+USERS[,score:=ifelse(u_occupation==2|u_occupation==3,score-30,score+30)] 
+
+USERS$score2 <- scale(USERS$score)
+
+USERS$treated <- sample(0:1,num_users,replace=T)
+
+
+USERS[,churn:=ifelse(score2>0,0,1)]
+# to do: switch for some ids 0 and 1
+
+
