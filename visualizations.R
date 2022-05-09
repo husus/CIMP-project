41e0561e4cd45a416d2e1077061d53a3726a5a77
Susan <Susan@MacBook-di-Susan.local>
2022-05-09 20:20:48 +0200

    Finalized code and comments

--- visualizations.R
+++ visualizations.R
@@ -69,54 +69,16 @@ for(perf in perf_list){
 
 
 # Plotting Qini Curves
-qini_plots <- MultiQiniPlots(perf_list, names=c('InterLogit', 'XGB', 'HCF'))
+qini_plots <- MultiQiniPlots(perf_list, names=c('InterLogit', 'XGB', 'HCF')) #personal function
 qini_plots[[1]]
 qini_plots[[2]]
 
-uplift_plot <- MovingDifference(perf_list,  names=c('InterLogit', 'XGB', 'HCF'))
+uplift_plot <- MovingDifference(perf_list,  names=c('InterLogit', 'XGB', 'HCF')) #personal function
 uplift_plot
 
 
-
-MovingDifference_mod <- function(performance_models, names, ngroups=10){
-  
-  # Plot the difference in y between treated and untreated units against the % of user
-  # base targeted starting from a list of performance objects of Tools4Uplift package
-  # Args:
-  #  performance_models: list of performance objects
-  #  names: names of the models in the same order as they were listed
-  #  ngroups: number of groups for computing performance
-  # Return:
-  #  plot: a plot showing how the estimated treatment effect changes 
-  #        when we increase the % of population targeted
-  
-  df <- data.frame(matrix(nrow=ngroups, ncol=length(performance_models)+1))
-  colnames(df) <- c('perc_target', names) 
-  for (j in seq(1:ngroups)){
-    df[j,1] <- round(j/ngroups, 2)}
-  
-  for (i in seq(1:length(performance_models))){
-    for(j in seq(1:ngroups)){
-      df[j, i+1] <- performance_models[[i]][[2]][j]/performance_models[[i]][[3]][j]-performance_models[[i]][[4]][j]/performance_models[[i]][[5]][j]
-    }
-  }
-  
-  df_perc_reshaped <- reshape2::melt(df, id.vars="perc_target", measure.vars=names)
-  
-  plot <- ggplot(data=df_perc_reshaped, aes(x=perc_target, y=value*100, colour=variable, group=variable)) +
-    geom_point() + geom_line(size=0.7, color="#E20812") + labs(title="Uplift vs % Customer Base Targeted", fill='Model', color='Model',
-                                      y='Uplift (pp)', x='Perc. of Customer Base Targeted')+mytheme+theme(legend.position="none")+scale_x_continuous(breaks=seq(0, 1, 0.1))+
-                                       geom_segment(x = 0.1, y=8.9, xend=1, yend=8.9, color="black",
-                                         linetype="dashed", size=0.5)
-    
-  
-  
-  return(plot)
-}
-
 perf_list <- list(perf_intermodel)
-
-uplift_plot <- MovingDifference_mod(perf_list, names=c( 'InterLogit'))
+uplift_plot <- MovingDifference(perf_list, names=c( 'InterLogit')) #personal function
 uplift_plot
 
 
@@ -174,64 +136,6 @@ ggplot(finale, aes(x=as.factor(u_other_sub))) +
   ylab('Number of Users')+mytheme+theme(legend.position="none")
 
 
-
-
-ResubPlots <- function(data, vars, target, treat){
-  
-  #Args:
-  #   data: dataset for the analysis
-  #   vars: list of categorical variables for which we need to 
-  #   evaluate differences in resub rates
-  #   target: name of target variable in the data
-  #   treat: name of treatment variable in the data
-  #   
-  # Return:
-  #   List of lists containing various graphs
-  
-  plot_list<-list()
-  plot_uplift<-list()
-  plot_groups<-list()
-  
-  
-  for(j in seq(1, length(vars))){
-    
-    values <- unique(data[, vars[j]])
-    data_plot <- data.frame(matrix(nrow=length(values), ncol=4))
-    colnames(data_plot) <- c("category", "treated", "untreated", "uplift")
-    k<-1
-    
-    for (i in seq(1, length(values))){
-      data_plot[i,1]=values[i]
-      data_sub_t=data[data[,vars[j]]==values[i]&data[,treat]==1,]
-      data_sub_c=data[data[,vars[j]]==values[i]&data[,treat]==0,]
-      data_plot[i,2]=round(as.numeric(table(data_sub_t[,target])/length(data_sub_t[,target]))[2],4)
-      data_plot[i,3]=round(as.numeric(table(data_sub_c[,target])/length(data_sub_c[,target]))[2],4)
-      data_plot[i,4]=data_plot[i,2]-data_plot[i,3]
-    }
-    
-    data_plot$category <- factor(data_plot$category)
-    pup <- ggplot(data=data_plot, aes(x=category, y=uplift)) +
-      geom_bar(stat='identity', col='black', fill='#E50914', alpha=0.7)+labs(title=sprintf("Uplift Across %s Categories", vars[j]), 
-                                                                  y="Uplift (%)", x=sprintf("%s Categories", vars[j]) )+mytheme
-    plot_uplift[[j]]=pup
-    
-    data.m <- reshape2::melt(data_plot, id.vars='category')
-    pgr=ggplot(data.m, aes(category, value)) + geom_bar(aes(fill = variable), 
-                                                        width = 0.4, position = position_dodge(width=0.5), 
-                                                        stat="identity") +
-      labs(title=sprintf("Uplift Across %s Categories", vars[j]), 
-            y="Perc. of  Resub", x=sprintf("%s Categories", vars[j]) ) +
-            mytheme + scale_fill_manual(values = c("#E50914", "#000000", "#564d4d"))
-    
-    plot_groups[[j]]=pgr
-    
-  }
-  
-  plot_list[[1]]=plot_uplift
-  plot_list[[2]]=plot_groups
-  
-  return(plot_list)
-}
 
 
 list_plot <- ResubPlots(data=finale, vars=c('u_age','u_occupation', 'u_plan', 'u_genre_pref', 'u_other_sub'), target='y', treat = 'treat')
